690ce33e546d403514bfbf03884658b3e9cded82
Timotei Dumitru <dumitru.timy@gmail.com>
2020-05-11 20:40:40 +0100

    implement the basic structure of the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./App.css";
+import LandingPage from "./components/landingPage/LandingPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Nespolous Design</h1>
-      </header>
+      <LandingPage />
     </div>
   );
 }
---
+++ src/components/landingPage/LandingPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./landing.scss";
+
+export default function LandingPage() {
+  return (
+    <div>
+      <h1>Hello from landing page</h1>
+    </div>
+  );
+}
