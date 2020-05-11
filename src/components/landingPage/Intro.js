705a05777b3676177428ea8f1ad1c88ed769b086
Timotei Dumitru <dumitru.timy@gmail.com>
2020-05-11 21:32:37 +0100

    add basic structure and style of intro section on landing page

---
+++ src/components/landingPage/Intro.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./landing.scss";
+
+export default function Intro() {
+  return (
+    <div className="main-intro">
+      <div className="pre-intro-text">
+        <h5>Los Encules Hermanos</h5>
+      </div>
+      <div className="intro-text">
+        <h3>
+          A L'aveugle on ne va <br />
+          nul part. faites <br />
+          parler vos marques
+        </h3>
+      </div>
+    </div>
+  );
+}
--- src/components/landingPage/Main.js
+++ src/components/landingPage/Main.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./landing.scss";
 import Header from "./Header";
+import Intro from "./Intro";
 
 export default function LandingPage() {
   return (
-    <div>
+    <div className="main-landing">
       <Header />
+      <Intro />
     </div>
   );
 }
